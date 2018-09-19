e5bbaeadc8af8a7d23a4ed9776f8a444f3b85bff
Trey Hall <linkin562@gmail.com>
2018-09-19 14:00:04 -0400

    restarted work on project

---
+++ LinkRPG/MainForm.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace UntitledRPG
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pbGameForm = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbGameForm)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbGameForm
+            // 
+            this.pbGameForm.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pbGameForm.Location = new System.Drawing.Point(0, 0);
+            this.pbGameForm.Name = "pbGameForm";
+            this.pbGameForm.Size = new System.Drawing.Size(800, 450);
+            this.pbGameForm.TabIndex = 0;
+            this.pbGameForm.TabStop = false;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pbGameForm);
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.MainForm_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pbGameForm)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbGameForm;
+    }
+}
---
+++ LinkRPG/MainForm.cs
@@ -0,0 +1,68 @@
+﻿using System.Windows.Forms;
+using LinkEngine.WorldGen;
+using 
+
+namespace UntitledRPG
+{
+    public partial class MainForm : Form
+    {
+        /// <summary>
+        /// Player is the user's controllable object
+        /// </summary>
+        Adventurer Player;
+        /// <summary>
+        /// world is the object that gets populated with information about the game
+        /// </summary>
+        World world;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        Adventurer CreatePlayer ()
+        {
+            return null
+        }
+        World CreateWorld()
+        {
+            return null;
+        }
+        void SetupWorld ()
+        {
+            world = CreateWorld();
+            Player = CreatePlayer();
+        }
+
+        #region Events
+        private void MainForm_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                // Movement Keys
+                case Keys.W:
+                    // move up
+                    Player.ShiftMap(0, 1);
+                    break;
+                case Keys.A:
+                    // move left
+                    Player.ShiftMap(-1, 0);
+                    break;
+                case Keys.S:
+                    // move down
+                    Player.ShiftMap(0, -1);
+                    break;
+                case Keys.D:
+                    // move right
+                    Player.ShiftMap(1, 0);
+                    break;
+
+                // Interaction Keys
+                case Keys.Enter:
+                    // interact
+                    break;
+            }
+        }
+        #endregion
+    }
+}
